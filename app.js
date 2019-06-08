bacd6829d50b92d38ddc310165fb1e050d62e9ba
Gamma9 <everythingbarstat@gmail.com>
2019-06-08 12:19:46 -0500

    First staging for Draft Picks Barbershop done

--- app.js
+++ app.js
@@ -1,65 +1,64 @@
-var createError = require( 'http-errors' );
-var express = require( 'express' );
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
-var cookieParser = require( 'cookie-parser' );
-var logger = require( 'morgan' );
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var indexRouter = require( './routes/index' );
-var usersRouter = require( './routes/users' );
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var athletesRouter = require('./routes/athletes');
+var aboutTjRouter = require('./routes/aboutTj');
+
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 // view engine setup
-app.set( 'views', path.join( __dirname, 'views' ) );
-app.set( 'view engine', 'ejs' );
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use( logger( 'dev' ) );
-app.use( express.json() );
+app.use(logger('dev'));
+app.use(express.json());
 
 //urlencoded
-app.use( express.urlencoded( {
+app.use(express.urlencoded({
 	extended: false
-} ) );
-app.use( bodyParser.urlencoded( {
+}));
+app.use(bodyParser.urlencoded({
 	extended: true
-} ) );
+}));
 
-app.use( cookieParser() );
-app.use( express.static( path.join( __dirname, 'public' ) ) );
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Mount routers
-app.use( '/', indexRouter );
-app.use( '/users', usersRouter );
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/athletes', athletesRouter);
+app.use('/aboutTj', aboutTjRouter);
 
 // catch 404 and forward to error handler
-app.use( function( req, res, next ) {
-	next( createError( 404 ) );
-} );
+app.use(function (req, res, next) {
+	next(createError(404));
+});
 
 // error handler
-app.use( function( err, req, res, next ) {
+app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
-	res.locals.error = req.app.get( 'env' ) === 'development' ? err : {};
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 	// render the error page
-	res.status( err.status || 500 );
-	res.render( 'error' );
-} );
-
-app.get( "/", function( req, res ) {
-	res.sendFile( "/public/index.html" );
-} );
-
-
+	res.status(err.status || 500);
+	res.render('error');
+});
 
 //==============================================================================
 
 //Begin listening on server
-app.listen( port, function( req, res ) {
-	console.log( "Server is listening on port: " + port );
-} );
+app.listen(port, function (req, res) {
+	console.log("Server is listening on port: " + port);
+});
 
 module.exports = app;
